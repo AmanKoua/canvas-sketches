f7086cdaeeafe9f324e497ee7186dfe62647c0f8
Aman <akoua@umich.edu>
2024-12-11 22:21:31 -0500

    more experimentation

--- quadratics.js
+++ quadratics.js
@@ -17,45 +17,80 @@ window.addEventListener("mousemove", (e) => {
 
 let mouseX = 0;
 let mouseY = 0;
-const COLS = 20;
+const COLS = 30;
 
 // Colors array of length numShades
 let colors = colorMap({
-  colormap:"cubehelix", // favs ['cool', 'spring', 'autumn', 'winter', 'bone', 'copper', 'bluered', 'blackbody', 'electric', 'warm', 'rainbow-soft', 'cubehelix']
+  colormap:"rainbow-soft", // favs ['cool', 'spring', 'autumn', 'winter', 'bone', 'copper', 'bluered', 'blackbody', 'electric', 'warm', 'rainbow-soft', 'cubehelix']
   nshades:COLS,
 });
 
 const sketch = () => {
 
+  const WIDTH_SLICE = WIDTH / COLS;
+
   const controlPoint = new Point();
+  const lines = [];
+  const lines2 = [];
+
+  for(let i = 0; i < COLS; i++){
+    lines.push(new Line(new Point(), new Point()))
+  }
+
+  for(let i = 0; i < COLS; i++){
+    lines2.push(new Line(new Point(), new Point()))
+  }
 
   return ({ context, width, height, frame }) => {
     
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    controlPoint.y = HEIGHT / 2;
-    controlPoint.x = (WIDTH * Math.sin(frame/20)) + WIDTH / 2;
+    controlPoint.x = ((WIDTH * Math.sin(frame/20)) + WIDTH / 2) + mouseX * 2;
+    controlPoint.y = mouseY * 5;
 
-    for(let i = 0; i < COLS; i++) {
+    // controlPoint.x += mouseX * 1;
+    // controlPoint.y += mouseY * 1;
 
-      let startX = i * (WIDTH / COLS) * Math.cos(frame/20);
-      let startY = 300 * (i / 3) * Math.sin(frame/20);
-      let destX = i * (WIDTH / COLS) * Math.sin(frame/30) + 500;
-      let destY =  HEIGHT - 50 * (i/1) * Math.cos(frame/20);
+    /*
+
+      V3 - Newest and coolest (arguably lol)
+
+    */
+    for(let i = 0; i < COLS; i++){
+
+      const line = lines[i];
+      const pointA = lines[i].pointA;
+      const pointB = lines[i].pointB;
+
+      const modX = (40 * i) * Math.cos(frame/20);
+      const modY = (60 * i) * Math.sin(frame/20);
+      const altModY = (50 * i ) * Math.sin(frame/40);
+
+      pointA.x = (i * WIDTH_SLICE) + WIDTH_SLICE / 2;
+      pointA.x += modX;
+
+      pointA.y =  (HEIGHT /8);
+      pointA.y += modY;
+
+      pointB.x = (i * WIDTH_SLICE) + WIDTH_SLICE / 2;
+      pointB.x += modY;
+
+      pointB.y =  HEIGHT - (HEIGHT /8);
+      pointB.y -= altModY;
 
       context.save();
 
-      context.translate(400,200);
+      context.translate(200,200);
       context.scale(.4,.4);
 
       context.beginPath();
-      context.moveTo(startX,startY);
-      context.quadraticCurveTo(controlPoint.x, controlPoint.y,destX,destY);
+      context.moveTo(pointA.x, pointA.y);
+      context.quadraticCurveTo(controlPoint.x, controlPoint.y,pointB.x, pointB.y);
       context.closePath();
 
       // context.globalCompositeOperation ="xor"; // xor
-      context.globalAlpha = 1;
+      context.globalAlpha = .2;
       context.fillStyle = colors[i];
       context.lineWidth= 5;
       context.strokeStyle = "black";
@@ -67,6 +102,93 @@ const sketch = () => {
 
     }
 
+    /*
+      V2 - The same, but kinda different. Still cool
+    */
+
+    // for(let i = 0; i < COLS; i++){
+
+    //   const pointA = lines2[i].pointA;
+    //   const pointB = lines2[i].pointB;
+
+
+    //   // const modX = (40 * i) * Math.cos(frame/20);
+    //   const modX = Math.pow((40 * i), Math.cos(frame/20));
+
+    //   const modY = (60 * i) * Math.sin(frame/20);
+    //   const altModY = (50 * i ) * Math.sin(frame/40);
+
+    //   pointA.x = (i * WIDTH_SLICE) + WIDTH_SLICE / 2;
+    //   pointA.x += modX;
+    //   pointA.x += 500; 
+
+    //   pointA.y =  (HEIGHT /8);
+    //   pointA.y += modY;
+    //   pointA.y += 500; 
+
+    //   pointB.x = (i * WIDTH_SLICE) + WIDTH_SLICE / 2;
+    //   pointB.x += modY;
+
+    //   pointB.y =  HEIGHT - (HEIGHT /8);
+    //   pointB.y -= altModY;
+
+    //   context.save();
+
+    //   context.translate(200,200);
+    //   context.scale(.4,.4);
+
+    //   context.beginPath();
+    //   context.moveTo(pointA.x, pointA.y);
+    //   context.quadraticCurveTo(controlPoint.x, controlPoint.y,pointB.x, pointB.y);
+    //   context.closePath();
+
+    //   // context.globalCompositeOperation ="xor"; // xor
+    //   context.globalAlpha = .2;
+    //   context.fillStyle = colors[i];
+    //   context.lineWidth= 5;
+    //   context.strokeStyle = "black";
+
+    //   context.fill();
+    //   context.stroke();
+
+    //   context.restore();
+
+    // }
+
+    /*
+      ORIGINAL IDEA. KINDA COOL
+    */
+
+    // for(let i = 0; i < COLS; i++) {
+
+    //   let startX = i * (WIDTH / COLS) * Math.cos(frame/20);
+    //   let startY = 300 * (i / 3) * Math.sin(frame/20);
+    //   let destX = i * (WIDTH / COLS) * Math.sin(frame/30) + 500;
+    //   let destY =  HEIGHT - 50 * (i/1) * Math.cos(frame/20);
+
+    //   context.save();
+
+    //   context.translate(400,200);
+    //   context.scale(.4,.4);
+
+    //   context.beginPath();
+    //   context.moveTo(startX,startY);
+    //   context.quadraticCurveTo(controlPoint.x, controlPoint.y,destX,destY);
+    //   context.closePath();
+
+    //   // context.globalCompositeOperation ="xor"; // xor
+    //   context.globalAlpha = 1;
+    //   context.fillStyle = colors[i];
+    //   context.lineWidth= 5;
+    //   context.strokeStyle = "black";
+
+    //   context.fill();
+    //   context.stroke();
+
+    //   context.restore();
+
+    // }
+
   };
 };
 
@@ -75,6 +197,15 @@ class Point {
   constructor(){
     this.x = 0;
     this.y = 0;
+  }
+
+}
+
+class Line {
+
+  constructor(pointA, pointB){
+    this.pointA = pointA;
+    this.pointB = pointB;
   }
 
 }
